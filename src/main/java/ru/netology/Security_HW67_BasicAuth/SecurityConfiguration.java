cc0d47027298822e246933342f01157df60049f1
Ekaterina Kononova <ekaterina.panova@mail.ru>
2024-01-09 15:32:17 +0500

    second task MethodSecurity

--- src/main/java/ru/netology/Security_HW67_BasicAuth/SecurityConfiguration.java
+++ src/main/java/ru/netology/Security_HW67_BasicAuth/SecurityConfiguration.java
@@ -1,29 +1,36 @@
 package ru.netology.Security_HW67_BasicAuth;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public PasswordEncoder encoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
                 .withUser("user1")
-                .password("{noop}password1")
-                .authorities("read")
+                .password(encoder().encode("password1"))
+                .roles("READ")
                 .and()
                 .withUser("user2")
-                .password("{noop}password2")
-                .authorities("write")
+                .password(encoder().encode("password2"))
+                .roles("WRITE")
                 .and()
                 .withUser("user3")
-                .password("{noop}password3")
-                .authorities("delete");
+                .password(encoder().encode("password3"))
+                .roles("DELETE");
     }
 
     @Override
@@ -32,22 +39,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/welcome").permitAll()
-                .and()
-                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/all").hasAuthority("read")
-                .and()
-                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/{id}").hasAuthority("read")
-                .and()
-                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/write").hasAuthority("write")
-                .and()
-                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/delete").hasAuthority("delete")
-                .and()
-
-                // для метода POST
-                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/posts").hasAuthority("write")
-                .and()
-
-                // для метода DELETE
-                .authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/posts/{id}").hasAuthority("delete")
                 .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
