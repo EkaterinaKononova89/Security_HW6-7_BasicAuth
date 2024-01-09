3e3137eaa35ed68a97ffaa9533fb176528b77639
Ekaterina Kononova <ekaterina.panova@mail.ru>
2024-01-09 16:44:49 +0500

    first_task_FilterChain

--- src/main/java/ru/netology/Security_HW67_BasicAuth/SecurityConfiguration.java
+++ src/main/java/ru/netology/Security_HW67_BasicAuth/SecurityConfiguration.java
@@ -2,44 +2,65 @@ package ru.netology.Security_HW67_BasicAuth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityConfiguration {
     @Bean
     public PasswordEncoder encoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("user1")
-                .password(encoder().encode("password1"))
-                .roles("READ")
-                .and()
-                .withUser("user2")
-                .password(encoder().encode("password2"))
-                .roles("WRITE")
-                .and()
-                .withUser("user3")
-                .password(encoder().encode("password3"))
-                .roles("DELETE");
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder encoder) {
+        UserDetails user1 = User.withUsername("user1")
+                .password(encoder.encode("password1"))
+                .authorities("read")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password(encoder.encode("password2"))
+                .authorities("write")
+                .build();
+        UserDetails user3 = User.withUsername("user3")
+                .password(encoder.encode("password3"))
+                .authorities("delete")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2, user3);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .formLogin()
                 .and()
                 .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/welcome").permitAll()
                 .and()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/all").hasAuthority("read")
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "/api/posts/{id}").hasAuthority("read")
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "api/posts/write").hasAuthority("write")
+                .and()
+                .authorizeRequests().antMatchers(HttpMethod.GET, "api/posts/delete").hasAuthority("delete")
+                .and()
+
+                // для метода POST
+                .authorizeRequests().antMatchers(HttpMethod.POST, "/api/posts").hasAuthority("write")
+                .and()
+
+                // для метода DELETE
+                .authorizeRequests().antMatchers(HttpMethod.DELETE, "/api/posts/{id}").hasAuthority("delete")
+
+                .and()
                 .authorizeRequests().anyRequest().authenticated();
+        return http.build();
     }
 }
--- src/main/java/ru/netology/Security_HW67_BasicAuth/controller/PostController.java
+++ src/main/java/ru/netology/Security_HW67_BasicAuth/controller/PostController.java
@@ -1,12 +1,9 @@
 package ru.netology.Security_HW67_BasicAuth.controller;
 
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.Security_HW67_BasicAuth.model.Post;
 import ru.netology.Security_HW67_BasicAuth.service.PostService;
 
-import javax.annotation.security.RolesAllowed;
 import java.util.List;
 
 @RestController
@@ -23,43 +20,31 @@ public class PostController {
         return "Welcome to our service! For more information you must log in";
     }
 
-    @PreAuthorize("#username == authentication.principal.username")
-    @GetMapping("/greeting")
-    public String greetingUser(@RequestParam String username) {
-        return "Welcome to our service, " + username;
-    }
-
-    @Secured({"ROLE_READ"})
     @GetMapping("/all")
     public List<Post> all() {
         return service.all();
     }
 
-    @Secured({"ROLE_READ"})
     @GetMapping("/{id}")
     public Post getById(@PathVariable long id) {
         return service.getById(id);
     }
 
-    @RolesAllowed({"ROLE_WRITE"})
     @GetMapping("/write")
     public String write() {
         return "authority write";
     }
 
-    @RolesAllowed({"ROLE_WRITE"})
     @PostMapping
     public Post save(@RequestBody Post post) {
         return service.save(post);
     }
 
-    @PreAuthorize("hasAnyRole('WRITE', 'DELETE')")
     @GetMapping("/delete")
     public String delete() {
         return "authority delete";
     }
 
-    @PreAuthorize("hasAnyRole('WRITE', 'DELETE')")
     @DeleteMapping("/{id}")
     public void removeById(@PathVariable long id) {
         service.removeById(id);
